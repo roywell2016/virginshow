db495e2a253aa82cb9cb0ae00950efaeb1cd8549
roywell <roywell@roywelldemacbook-pro.local>
2016-09-16 10:54:46 +0800

    Initial Commit

---
+++ virgin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  virgin
+//
+//  Created by roywell on 16/9/16.
+//  Copyright © 2016年 roywell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ virgin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  virgin
+//
+//  Created by roywell on 16/9/16.
+//  Copyright © 2016年 roywell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
